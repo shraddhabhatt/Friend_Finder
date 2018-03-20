dff8d6b579b2889459d0ea6d32e41b31a31a396e
shraddhabhatt <trivedishraddha99@gmail.com>
2018-03-19 23:49:16 -0400

    Final commit

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Shraddha",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [4, 3, 2, 4, 1, 4, 5, 4, 2, 1]
+  },
+  {
+    name: "Samuel",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 2, 1, 3, 1, 4, 3, 1, 5, 3]
+  }
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,86 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources. 
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData 	= require('../data/friends.js');
+// var waitListData 	= require('../data/waitinglist-data.js');
+// var path 			= require('path');
+
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app){
+
+	// API GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases when a user visits a link 
+	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
+	// ---------------------------------------------------------------------------
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendsData);
+	});
+
+	// app.get('/api/waitlist', function(req, res){
+	// 	res.json(waitListData);
+	// });
+
+	// API POST Requests
+	// Below code handles when a user submits a form and thus submits data to the server.
+	// In each of the below cases, when a user submits form data (a JSON object)
+	// ...the JSON is pushed to the appropriate Javascript array
+	// (ex. User fills out a reservation request... this data is then sent to the server...
+	// Then the server saves the data to the tableData array)
+	// ---------------------------------------------------------------------------
+
+	app.post('/api/friends', function(req, res){
+
+		var newUser = req.body;
+        var differences = [];
+        if (friends.length < 1) {
+            console.log("unable to do calculation; not enough users");
+        } else {
+            compareFriends(friends, newUser, differences);
+            var lowest = differences[0];
+            for (var i = 0; i < differences.length; i++) {
+                if (differences[i] < lowest) {
+                    lowest = differences[i];
+                }
+            };
+            var bestMatch = differences.indexOf(lowest);
+            res.send(friends[bestMatch]);
+        };
+        friends.push(newUser);
+	});
+
+
+	function compareFriends(friends, newUser, differences) {
+	    var curUserIndex = 0;
+	    while (curUserIndex < friends.length) {
+	        var totalDifference = 0;
+	        for (var i = 0; i < newUser.scores.length; i++) {
+	            totalDifference += Math.abs(parseInt(friends[curUserIndex].scores[i]) - parseInt(newUser.scores[i]));
+	        }
+	        differences.push(totalDifference);
+	        curUserIndex++;
+	    }
+	}
+
+	// ---------------------------------------------------------------------------
+	// I added this below code so you could clear out the table while working with the functionality.
+	// Don't worry about it!
+
+	// app.post('/api/clear', function(req, res){
+	// 	// Empty out the arrays of data
+	// 	friendsData = [];
+	// 	waitListData = [];
+
+	// 	console.log(tableData);
+	// })
+}
